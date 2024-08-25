37baf874b3805c99166d92a86189f1d2b03969ef
truc <tructhanh2k3@gmail.com>
2024-08-26 00:03:11 +0700

    fix thanh toan

--- src/thanhtoan/Btl_pass.java
+++ src/thanhtoan/Btl_pass.java
@@ -7,16 +7,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Btl_pass {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "D:\\DowloadApp\\autoTest\\chromedriver-win64\\chromedriver.exe");
-
         WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));  // Tăng thời gian đợi
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
         try {
             driver.get("https://pgmgolf.vn/");
@@ -32,15 +30,21 @@ public class Btl_pass {
             wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".content:nth-child(7)"))).click();
             TimeUnit.SECONDS.sleep(2);
 
-            driver.findElement(By.id("billing_address_full_name")).sendKeys("Thuy Mai");
+             //Điền thông tin thanh toán
+            WebElement nameField = wait.until(ExpectedConditions.elementToBeClickable(By.id("billing_address_full_name")));
+            nameField.sendKeys("thuy hoai");
+            TimeUnit.SECONDS.sleep(2);
 
-            driver.findElement(By.id("checkout_user_email")).sendKeys("thanhthanh@gmail.com");
-            driver.findElement(By.id("billing_address_phone")).sendKeys("0368479525");
+            WebElement emailField = wait.until(ExpectedConditions.elementToBeClickable(By.id("checkout_user_email")));
+            emailField.sendKeys("thanhthanh@");
+            TimeUnit.SECONDS.sleep(2);
 
-            wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".section-customer-information > .fieldset"))).click();
-
+            WebElement phoneField = wait.until(ExpectedConditions.elementToBeClickable(By.id("billing_address_phone")));
+            phoneField.sendKeys("0368479525");
+            TimeUnit.SECONDS.sleep(2);
+            
             // Nhập địa chỉ
-            WebElement addressField = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("billing_address_address1")));
+            WebElement addressField = wait.until(ExpectedConditions.elementToBeClickable(By.id("billing_address_address1")));
             addressField.sendKeys("nghe thanh");
             TimeUnit.SECONDS.sleep(2);
 
@@ -73,13 +77,74 @@ public class Btl_pass {
             paymentMethod.click();
             TimeUnit.SECONDS.sleep(2);
 
-            WebElement thanhToan = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"checkout_complete\"]/button")));
-            thanhToan.click();
-            TimeUnit.SECONDS.sleep(2);
+            // Nhấn nút thanh toán
+            //để trống trường bắt buộc
+            WebElement thanhToan = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"form_next_step\"]/button")));
 
-            String thongBaoThanhCong =  driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div/div[1]/div/div/h2")).getText();
-            System.out.println(thongBaoThanhCong);
-            TimeUnit.SECONDS.sleep(2);
+//            //Nhập thông tin hợp lệ
+//            WebElement thanhToan = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"checkout_complete\"]/button")));
+            thanhToan.click();
+            TimeUnit.SECONDS.sleep(5);
+            
+            try {
+                // Kiểm tra thông báo lỗi cho các trường bắt buộc
+                boolean hasError = false;
+
+                // nhóm lỗi để trống
+                String[] errorXPaths = {
+                    "/html/body/div[3]/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/div/div[1]/p", // Tên
+                    "/html/body/div[3]/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/div/div[3]/p", // Số điện thoại
+                    "//*[@id=\"form_update_location_customer_shipping\"]/div[2]/p", // Địa chỉ
+                    "//*[@id=\"form_update_location_customer_shipping\"]/div[3]/p", // Tỉnh
+                    "//*[@id=\"form_update_location_customer_shipping\"]/div[4]/p", // Quận
+                    "//*[@id=\"form_update_location_customer_shipping\"]/div[5]/p" // Xã
+                };
+
+                // Kiểm tra từng thông báo lỗi
+                for (String xpath : errorXPaths) {
+                    try {
+                        WebElement errorElement = driver.findElement(By.xpath(xpath));
+                        if (errorElement.isDisplayed()) {
+                            System.out.println("Vui long nhạp cac truong bat buoc!!");
+                            hasError = true;
+                            break; // Dừng kiểm tra nếu đã phát hiện lỗi
+                        }
+                    } catch (Exception e) {
+                        // Lỗi khi không tìm thấy phần tử không cần xử lý
+                    }
+                }
+
+                // Kiểm tra lỗi email không đúng định dạng
+                if (!hasError) {
+                    try {
+                        WebElement emailError = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/div/div[2]/p"));
+                        if (emailError.isDisplayed()) {
+                            System.out.println("Email khong dung dinh dang!!");
+                            hasError = true;
+                        }
+                    } catch (Exception e) {
+                        // Lỗi khi không tìm thấy phần tử không cần xử lý
+                    }
+                }
+
+                // Kiểm tra thông báo thanh toán thành công nếu không có lỗi
+                if (!hasError) {
+                    try {
+                        WebElement thongBaoThanhCong = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div/div[1]/div/div/h2"));
+                        if (thongBaoThanhCong.isDisplayed()) {
+                            System.out.println("Thanh toan thanh cong!!!");
+                        } else {
+                            System.out.println("Thanh toan khong thanh cong!!!!!");
+                        }
+                    } catch (Exception ex) {
+                        System.out.println("Co loi xay ra!");
+                        ex.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
